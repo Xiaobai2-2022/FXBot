6f2ddf086b5975d55eba5c590815a861ca3f9814
Xiaobai2-2022 <z2367li@uwaterloo.ca>
2025-03-28 12:02:47 +0800

    Added ValidateKey with @FXValidateKey Simplified Security Key Check.

---
+++ src/main/java/com/fangxia/fxbot/aop/FXValidateKeyAOP.java
@@ -0,0 +1,41 @@
+package com.fangxia.fxbot.aop;
+
+import com.fangxia.fxbot.annotations.FXValidateKey;
+import com.fangxia.fxbot.common.FXApiResponse;
+import com.fangxia.fxbot.security.FXSecurityKey;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class FXValidateKeyAOP {
+
+    private final FXSecurityKey fxSecurityKey;
+
+    @Around("@annotation(fxValidateKey)")
+    public Object verifyServerKey(ProceedingJoinPoint joinPoint, FXValidateKey fxValidateKey) throws Throwable {
+        ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attrs == null) {
+            return FXApiResponse.failure("No request context found.");
+        }
+
+        HttpServletRequest request = attrs.getRequest();
+        String serverKey = request.getHeader("serverKey");
+
+        FXApiResponse<?> result = fxSecurityKey.validate(serverKey);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        return joinPoint.proceed();
+    }
+
+}
--- src/main/java/com/fangxia/fxbot/controller/FXUserChannelController.java
+++ src/main/java/com/fangxia/fxbot/controller/FXUserChannelController.java
@@ -1,8 +1,8 @@
 package com.fangxia.fxbot.controller;
 
+import com.fangxia.fxbot.annotations.FXValidateKey;
 import com.fangxia.fxbot.common.FXApiResponse;
 import com.fangxia.fxbot.dto.FXUserChannelDTO;
-import com.fangxia.fxbot.security.FXSecurityKey;
 import com.fangxia.fxbot.service.FXUserChannelService;
 
 import lombok.RequiredArgsConstructor ;
@@ -15,36 +15,23 @@ import org.springframework.web.bind.annotation.*;
 public class FXUserChannelController {
 
     private final FXUserChannelService fxUserChannelService;
-    private final FXSecurityKey fxSecurityKey;
 
     @PostMapping("/create")
-    public FXApiResponse<?> createChannel(
-        @RequestBody FXUserChannelDTO fxUserChannelDTO,
-        @RequestHeader(required = false) String serverKey
-    ) {
-        FXApiResponse<?> securityCheckDTO = fxSecurityKey.validate(serverKey);
-        if(securityCheckDTO.isSuccess()) {
-            if(fxUserChannelService.createChannel(fxUserChannelDTO) > 0) {
-                return FXApiResponse.success(null);
-            }
-            return FXApiResponse.failure("Unable to create channel.");
+    @FXValidateKey
+    public FXApiResponse<?> createChannel(@RequestBody FXUserChannelDTO fxUserChannelDTO) {
+        if(fxUserChannelService.createChannel(fxUserChannelDTO) > 0) {
+            return FXApiResponse.success(null);
         }
-        return securityCheckDTO;
+        return FXApiResponse.failure("Unable to create channel.");
     }
 
     @PutMapping("/update")
-    public FXApiResponse<?> updateChannel(
-            @RequestBody FXUserChannelDTO fxUserChannelDTO,
-            @RequestHeader(required = false) String serverKey
-    ) {
-        FXApiResponse<?> securityCheckDTO = fxSecurityKey.validate(serverKey);
-        if(securityCheckDTO.isSuccess()) {
-            if(fxUserChannelService.updateChannel(fxUserChannelDTO) > 0) {
-                return FXApiResponse.success(null);
-            }
-            return FXApiResponse.failure("Unable to create channel.");
+    @FXValidateKey
+    public FXApiResponse<?> updateChannel(@RequestBody FXUserChannelDTO fxUserChannelDTO) {
+        if(fxUserChannelService.updateChannel(fxUserChannelDTO) > 0) {
+            return FXApiResponse.success(null);
         }
-        return securityCheckDTO;
+        return FXApiResponse.failure("Unable to create channel.");
     }
 
 }
